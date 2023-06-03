d7e2be521a15d9e4fc98a9decf597e57753ce551
dileepk706 <dlpkmr706@gmail.com>
2023-06-03 12:05:48 +0530

    todo app completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,11 @@
 
-export const Todo = ({ task ,toggleComplete,deleteTodo}) => {
+export const Todo = ({ task ,toggleComplete,deleteTodo, isUpdate}) => {
 
     return (
         <div className="todo">
             <p className={task.isComplete?'complete':""} onClick={()=>toggleComplete(task.id)}>{task.task}</p>
             <div className="btn">
-                <button>update</button>
+                <button onClick={()=>isUpdate(task.id)}>update</button>
                 <button onClick={()=>deleteTodo(task.id)}>delete</button>
             </div>
 
